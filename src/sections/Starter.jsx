6c31e0e2e107e47b8d1b6ac5c70aed1f775a3632
Thomas Chen <thomaschen333@gmail.com>
2022-01-22 22:37:00 +1100

    Added icons that redirect to github and resume

--- src/sections/Starter.jsx
+++ src/sections/Starter.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import Button from "../components/Button";
 import { useScroll } from "../components/useScroll";
-import { HiOutlineArrowNarrowRight } from "react-icons/hi";
-import { GoPlay } from "react-icons/go";
 import { motion } from "framer-motion";
-import WorkImage from "../assets/work.svg";
 import "../styles/sections/Starter.scss";
 import { headerAnimation, imageAnimation } from "../Animation";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { MdOutlineContactPage } from "react-icons/md";
 
 export default function Starter() {
   const [element, controls] = useScroll();
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Student", "Developer", "Casual Academic"],
+    });
+  }, []);
 
   return (
     <div className="main-container" ref={element}>
-      <Navbar />
       <div className="container">
         <motion.div
           className="content"
@@ -22,31 +29,33 @@ export default function Starter() {
           animate={controls}
           transition={{ delay: 0.2, type: "tween" }}
         >
-          <h1>
-            We Provide Solutions to Help You to Build or Grow Your Business!
-          </h1>
-          <p>
-            A professional web and mobile app development agency with over 100+
-            web and app developed. We provide a high- quality service in web and
-            mobile app development as well as in design.
-          </p>
-          <div className="button-container">
-            <Button content="Watch Video" icon={<GoPlay />} />
-            <Button
-              color="pink"
-              content="Request Quote"
-              icon={<HiOutlineArrowNarrowRight />}
-            />
+          <h1>Thomas Chen</h1>
+          <h2>
+            <span ref={textRef}></span>
+          </h2>
+        </motion.div>
+        <div className="row-container">
+          <div className="section1">
+            <p>Bachelor of Commerce/Computer Science</p>
+            <p className="dean">Dean's List - Highly Commended</p>
           </div>
-        </motion.div>
-        <motion.div
-          className="image"
-          variants={imageAnimation}
-          animate={controls}
-          transition={{ type: "tween" }}
-        >
-          <img src={WorkImage} alt="Work Image" />
-        </motion.div>
+          <div className="vertical"></div>
+          <div className="section2">
+            <p>University of New South Wales</p>
+          </div>
+        </div>
+        <div className="contact-container">
+          <div
+            className="github"
+            onClick={() => window.open("https://github.com/tc3149", "mywindow")}
+            style={{ cursor: "pointer" }}
+          >
+            <AiFillGithub color="white" size={"2rem"} />
+          </div>
+          <div className="resume" style={{ cursor: "pointer" }}>
+            <MdOutlineContactPage color="white" size={"2rem"} />
+          </div>
+        </div>
       </div>
     </div>
   );
